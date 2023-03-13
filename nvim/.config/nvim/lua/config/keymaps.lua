9fe32427f8ae0770ce3a1b6594123fc88eeee78d
Peter Vilim <pvilim@dropbox.com>
2023-03-12 22:58:59 -0700

    m

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -15,3 +15,26 @@ map("n", "<C-w>h", ":vsplit<CR>", { desc = "Open split left" })
 map("n", "<C-w>j", ":split<CR>", { desc = "Open split down" })
 map("n", "<C-w>k", ":split<CR>", { desc = "Open split up" })
 map("n", "<C-w>l", ":vsplit<CR>", { desc = "Open split right" })
+
+map("n", "K", "<C-B>", { desc = "Page Up" })
+map("n", "J", "<C-F>", { desc = "Page Down" })
+
+map("n", "<leader>w/", ":cd /<CR>", { desc = "Navigate to fs root" })
+map("n", "<leader>w.", ":cd.. <CR>", { desc = "Navigate up a directory" })
+map("n", "<leader>wc", ":cd ", { desc = "Navigate to directory" })
+map("n", "<leader>wH", ":cd /Users/pvilim/hacks/pvilim<CR>", { desc = "Navigate to hacks" })
+map("n", "<leader>wh", ":cd /Users/pvilim/<CR>", { desc = "Navigate to home" })
+map("n", "<leader>wd", ":cd /Users/pvilim/client/desktop<CR>", { desc = "Navigate to desktop" })
+map("n", "<leader>wr", ":cd /Users/pvilim/client/desktop/rust<CR>", { desc = "Navigate to rust" })
+map("n", "<leader>wn", ":cd /Users/pvilim/client/desktop/rust/nucleus<CR>", { desc = "Navigate to nucleus" })
+map("n", "<leader>ws", ":cd /Users/pvilim/server<CR>", { desc = "Navigate to server" })
+map("n", "<leader>wg", ":cd /Users/pvilim/server/go<CR>", { desc = "Navigate to go" })
+map("n", "<leader>wp", ":cd /Users/pvilim/server/configs/proto<CR>", { desc = "Navigate to protos" })
+map("n", "<leader>wm", ":cd /Users/pvilim/server/metaserver/metaservlets<CR>", { desc = "Navigate to metaservlets" })
+map(
+  "n",
+  "<leader>wi",
+  ":cd /Users/pvilim/server/go/src/dropbox/devtools/image_builder<CR>",
+  { desc = "Navigate to image_builder" }
+)
+map("n", "<leader>wj", ":cd /Users/pvilim/server/metaserver/static/js<CR>", { desc = "Navigate to js" })
