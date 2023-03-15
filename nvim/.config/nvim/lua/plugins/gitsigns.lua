86d6d9922fd78f3239bd579961c47b6d009e0eab
Peter Vilim <pvilim@dropbox.com>
2023-03-14 23:06:48 -0700

    m

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -15,16 +15,21 @@ return {
     opts = function()
       -- Default to HEAD
       vim.g["git_base"] = "HEAD"
+      function toggle_base(new_base)
+        vim.g["git_base"] = new_base
+        require("gitsigns").change_base(vim.g["git_base"], true)
+      end
+
       map(
         "n",
         "<leader>gM",
-        ":lua require('gitsigns').change_base(vim.g['git_base'], true)<CR>",
+        ":lua toggle_base('origin/master')<CR>:lua toggle_file_viewer(false)<CR>",
         { desc = "Git sign diff vs origin/master" }
       )
       map(
         "n",
         "<leader>gH",
-        ":lua require('gitsigns').change_base(vim.g['git_base'], true)<CR>",
+        ":lua toggle_base('HEAD')<CR>:lua toggle_file_viewer(false)<CR>",
         { desc = "Git sign diff vs HEAD" }
       )
     end,
--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,11 +6,20 @@ return {
         bind_to_cwd = true,
       }
       local command = require("neo-tree.command")
-      function toggle_file_viewer()
-        command.execute({ toggle = true, action = "show", source = "filesystem", git_base = vim.g["git_base"] })
+      vim.g["neo_tree_source"] = "fileystem"
+      function toggle_neo_tree(toggle, source)
+        vim.g["neo_tree_source"] = source
+
+        command.execute({
+          toggle = toggle,
+          action = "show",
+          source = vim.g["neo_tree_source"],
+          git_base = vim.g["git_base"],
+        })
       end
 
-      vim.keymap.set("n", "<leader>e", ":lua toggle_file_viewer()<CR>", { desc = "Open file viewer" })
+      vim.keymap.set("n", "<leader>e", ":lua toggle_neo_tree(true, 'filesystem')<CR>", { desc = "Open file viewer" })
+      vim.keymap.set("n", "<leader>E", ":lua toggle_neo_tree(true, 'git_status')<CR>", { desc = "Open file viewer" })
     end,
   },
 }
