74b4d7f33661cc5b8f25a090a10a627e742054c0
Peter Vilim <pvilim@dropbox.com>
2023-05-05 19:21:14 -0700

    test

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -4,6 +4,12 @@ return {
     opts = {
         load = {
             ["core.defaults"] = {}, -- Loads default behaviour
+            ["core.keybinds"] = {
+                config = {
+                    default_keybinds = true,
+                    neorg_leader = "<Leader><Leader>"
+                }
+            }, -- Loads default behaviour
             ["core.concealer"] = {}, -- Adds pretty icons to your documents
             ["core.dirman"] = { -- Manages Neorg workspaces
                 config = {workspaces = {notes = "~/notes"}}
