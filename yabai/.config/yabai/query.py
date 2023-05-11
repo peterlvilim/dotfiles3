2b5622037cc25b28e5b412367328e160789421ed
Peter Vilim <pvilim@dropbox.com>
2023-05-10 20:19:30 -0700

    m

--- yabai/.config/yabai/query.py
+++ yabai/.config/yabai/query.py
@@ -3,28 +3,28 @@ import subprocess
 import plistlib
 
 def current_space():
-    return json.loads(subprocess.check_output("yabai -m query --spaces --space", shell=True))
+    return json.loads(subprocess.check_output("/usr/local/bin/yabai -m query --spaces --space", shell=True))
 
 def get_all_spaces():
-    return json.loads(subprocess.check_output("yabai -m query --spaces", shell=True))
+    return json.loads(subprocess.check_output("/usr/local/bin/yabai -m query --spaces", shell=True))
 
 def get_all_windows():
-    return json.loads(subprocess.check_output("yabai -m query --windows", shell=True))
+    return json.loads(subprocess.check_output("/usr/local/bin/yabai -m query --windows", shell=True))
 
 def get_windows_current_space():
-    return json.loads(subprocess.check_output("yabai -m query --windows --space", shell=True))
+    return json.loads(subprocess.check_output("/usr/local/bin/yabai -m query --windows --space", shell=True))
 
 def get_current_window():
-    return json.loads(subprocess.check_output("yabai -m query --windows --window", shell=True))
+    return json.loads(subprocess.check_output("/usr/local/bin/yabai -m query --windows --window", shell=True))
 
 def kill_process(pid):
     subprocess.check_output("kill -9 {}".format(pid), shell=True)
 
 def close_space(id):
-    subprocess.check_output("yabai -m space --destroy {}".format(id), shell=True)
+    subprocess.check_output("/usr/local/bin/yabai -m space --destroy {}".format(id), shell=True)
 
 def create_space():
-    subprocess.check_output("yabai -m space --create", shell=True)
+    subprocess.check_output("/usr/local/bin/yabai -m space --create", shell=True)
 
 def testing():
     return plistlib.loads(subprocess.check_output("defaults read com.apple.spaces", shell=True))
