91e2b970b5d23fcd2e367b568ed7a62c3003169a
Peter Vilim <pvilim@dropbox.com>
2023-05-10 20:32:07 -0700

    m

---
+++ ubersicht/Library/Application Support/Übersicht/widgets/top-bar/index.jsx
@@ -0,0 +1,172 @@
+import { css } from "uebersicht";
+
+const base03 = "#002b36";
+const white = "#eee8d5";
+const base01 = "#586e75";
+const base3 = "#fdf6e3";
+const green = "#859900";
+
+const workspaces = css({
+  display: "flex",
+  justifyContent: "flex-start",
+  paddingLeft: "10px",
+});
+
+const main = css({
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "row",
+  height: "38px",
+  width: "2056px",
+  backgroundColor: base03,
+  fontFamily: "Verdana",
+  fontSize: "10px",
+});
+
+const workspaceEnabled = css({
+  backgroundColor: green,
+  color: white,
+  borderColor: white,
+});
+
+const workspaceDisabled = css({
+  backgroundColor: base03,
+  color: base01,
+  borderColor: base01,
+});
+
+const workspace = css({
+  height: "12px",
+  margin: "5px",
+  padding: "3px",
+  borderWidth: "1px",
+  borderStyle: "solid",
+});
+
+const timeOuter = css({
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+  padding: "5px",
+});
+
+const dateGroupOuter = css({
+  flexGrow: "1",
+  paddingRight: "10px",
+  display: "flex",
+  justifyContent: "flex-end",
+  flexDirection: "row",
+  color: base01,
+});
+const dateGroupInner = css({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+});
+
+// print timestamps to debug
+export const refreshFrequency = 50;
+
+export const command = `./top-bar/target/debug/status`;
+
+// todo handle errors?
+export const updateState = (command_output, previousState) => {
+  const new_state = JSON.parse(command_output.output);
+  return new_state;
+};
+
+function Workspace({ index, label, enabled }) {
+  return (
+    <div
+      key={index}
+      className={`${workspace} ${
+        enabled ? workspaceEnabled : workspaceDisabled
+      }`}
+    >
+      {index} - {label}
+    </div>
+  );
+}
+
+function DateContainer({ weekday, month, day, times }) {
+  return (
+    <div className={dateGroupInner}>
+      <div>
+        {weekday} {month.toString().padStart(2, "0")}/
+        {day.toString().padStart(2, "0")}
+      </div>
+      {times.map((time) => (
+        <TimeDisplay
+          label={time.label}
+          hour={time.hour.toString().padStart(2, "0")}
+          minute={time.minute.toString().padStart(2, "0")}
+        />
+      ))}
+    </div>
+  );
+}
+
+function TimeDisplay({ label, hour, minute }) {
+  return (
+    <div key={label} className={timeOuter}>
+      <div>{label}</div>
+      <div>
+        {hour}:{minute}
+      </div>
+    </div>
+  );
+}
+
+export const render = (new_state, error) => {
+  //return <div />;
+  if (new_state.spaces === undefined) {
+    return <div />;
+  }
+
+  return (
+    <div className={main}>
+      <div className={workspaces}>
+        {new_state.spaces.map((space) => (
+          <Workspace
+            key={space.index}
+            index={space.index}
+            label={space.label}
+            enabled={space.enabled}
+          />
+        ))}
+      </div>
+      <div className={dateGroupOuter}>
+        <DateContainer
+          weekday={new_state.date_group_1.weekday}
+          month={new_state.date_group_1.month}
+          day={new_state.date_group_1.day}
+          times={new_state.date_group_1.times}
+        />
+        <DateContainer
+          weekday={new_state.date_group_2.weekday}
+          month={new_state.date_group_2.month}
+          day={new_state.date_group_2.day}
+          times={new_state.date_group_2.times}
+        />
+      </div>
+    </div>
+  );
+};
+
+// todo
+// move bar grouping to right
+// make use of classes for css
+// fix not selected color
+// fix selected color
+// fix font
+//
+
+// todo
+// workspaces with numbers by them
+// battery percent
+// network state?
+// clocks for various time zones
+//
+// other:
+// unread notifications for gmail, slack, etc
+// next calendar event?
---
+++ ubersicht/Library/Application Support/Übersicht/widgets/top-bar/src/main.rs
@@ -0,0 +1,127 @@
+use chrono_tz::Tz;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::process::Command;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DateGroup {
+    month: u32,
+    day: u32,
+    weekday: String,
+    times: Vec<OutputTime>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct OutputTime {
+    label: String,
+    hour: u32,
+    minute: u32,
+}
+use chrono_tz;
+
+use chrono::{DateTime, Datelike, Local, Offset, TimeZone, Timelike, Utc, Weekday};
+
+fn get_date_time(tzs: Vec<(String, Tz)>) -> (DateGroup, DateGroup) {
+    let mut group_one = DateGroup {
+        month: 0,
+        day: 0,
+        weekday: Weekday::Mon.to_string(),
+        times: vec![],
+    };
+    let mut group_two = DateGroup {
+        month: 0,
+        day: 0,
+        weekday: Weekday::Mon.to_string(),
+        times: vec![],
+    };
+    for (label, tz) in tzs {
+        let now = Utc::now().with_timezone(&tz);
+        if group_one.day == 0 {
+            group_one.month = now.month();
+            group_one.day = now.day();
+            group_one.weekday = now.weekday().to_string();
+        } else if group_two.day == 0 && now.day() != group_one.day {
+            group_two.month = now.month();
+            group_two.day = now.day();
+            group_two.weekday = now.weekday().to_string();
+        }
+        if now.day() == group_one.day {
+            assert_eq!(now.month(), group_one.month);
+            assert_eq!(now.weekday().to_string(), group_one.weekday);
+            group_one.times.push(OutputTime {
+                label,
+                hour: now.hour(),
+                minute: now.minute(),
+            });
+        } else if now.day() == group_two.day {
+            assert_eq!(now.month(), group_two.month);
+            assert_eq!(now.weekday().to_string(), group_two.weekday);
+            group_two.times.push(OutputTime {
+                label,
+                hour: now.hour(),
+                minute: now.minute(),
+            });
+        } else {
+            unreachable!();
+        }
+    }
+    group_one.times.sort_by(|a, b| a.hour.cmp(&b.hour));
+    group_two.times.sort_by(|a, b| a.hour.cmp(&b.hour));
+    if group_one.day <= group_two.day {
+        (group_one, group_two)
+    } else {
+        (group_two, group_one)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Output {
+    spaces: Vec<Workspace>,
+    date_group_1: DateGroup,
+    date_group_2: DateGroup,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Workspace {
+    index: u64,
+    label: String,
+    enabled: bool,
+}
+
+fn get_workspaces() -> Vec<Workspace> {
+    let mut workspaces = vec![];
+    let output = &Command::new("/usr/local/bin/yabai")
+        .args(&["-m", "query", "--spaces"])
+        .output()
+        .expect("failed to execute process")
+        .stdout;
+    let output = String::from_utf8_lossy(output);
+    let data: Value = serde_json::from_str(&output).expect("decode failed");
+    for space in data.as_array().unwrap().iter() {
+        let index = space.get("index").unwrap().as_u64().unwrap();
+        let label = space.get("label").unwrap().as_str().unwrap().to_string();
+        let enabled = space.get("is-visible").unwrap().as_bool().unwrap();
+        workspaces.push(Workspace {
+            index,
+            label,
+            enabled,
+        });
+    }
+    return workspaces;
+}
+
+fn main() {
+    let workspaces = get_workspaces();
+    let (date_group_1, date_group_2) = get_date_time(vec![
+        (String::from("SFO"), chrono_tz::US::Pacific),
+        (String::from("UTC"), chrono_tz::UTC),
+        (String::from("SIN"), chrono_tz::Asia::Singapore),
+        (String::from("PAR"), chrono_tz::Europe::Paris),
+        (String::from("ICN"), chrono_tz::Asia::Seoul),
+    ]);
+    let output = Output {
+        spaces: workspaces,
+        date_group_1,
+        date_group_2,
+    };
+    println!("{}", serde_json::to_string(&output).unwrap())
+}
